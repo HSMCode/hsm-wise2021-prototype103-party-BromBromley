2c3baa2746a1fef266194a771d26d1cfb3ad2e99
ldacruz <bitbucket@lindakruse.de>
2022-01-12 12:20:47 +0100

    update head rotation call
    set bool to define when timer is running
    adjust if statements for timer count to have valid true states
    start coroutine when if statement is true
    reset head rotation to zero, when coroutine starts

--- Assets/Scripts/OpponentBehaviour.cs
+++ Assets/Scripts/OpponentBehaviour.cs
@@ -1,6 +1,8 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class OpponentBehaviour : MonoBehaviour
 {
@@ -9,56 +11,69 @@ public class OpponentBehaviour : MonoBehaviour
     public GameObject head;
     private float rotationSpeed = 10;
     public float rotationAngle;
+    public bool countdown;
     void Start()
     {
-        timer = Random.Range(5, 15);
+        timer = Random.Range(8, 10);
+        countdown = true;
     }
 
     void Update()
     {
         //tells the opponent to look away once the timer hits zero, but leads to their head spinning uncontrollably instead of facing the front again
         // none of the attempts to fix it have worked so far
-        lookingAwayTimer();
-        Debug.Log(timer);
+        LookingAwayTimer();
+        // Debug.Log(timer);
 
 
-        if (timer <= 0) //head rotates, but can't stand still or face the player
+        if (timer > 1 && timer < 5 && countdown) //head rotates, but can't stand still or face the player
         {
-            head.transform.Rotate(new Vector3(0, 90, 0) * rotationSpeed * Time.deltaTime, Space.World);
+            head.transform.Rotate(new Vector3(0, (90 * rotationSpeed) * Time.deltaTime, 0), Space.World);
             Debug.Log("head starts spinning uncontrollably"); //because the timer stays below 0
         }
 
-        /* nested loops to rotate the head
-        if (timer >= 0 && timer <= 1)
+        // nested loops to rotate the head
+        else if (timer < 1 && countdown)
         {
-            //StartCoroutine (wait());
-            head.transform.Rotate(new Vector3(0, 90, 0) * rotationSpeed * Time.deltaTime, Space.World);
-            if (timer <= -3)
-            {
-                Debug.Log("timer hit -3");
-                head.transform.Rotate(new Vector3(0, -90, 0) * rotationSpeed * Time.deltaTime, Space.World);
-                timer = Random.Range(5, 15);
-            }
-        }*/
-
-        /* coroutine to rotate the head
-        if (timer >= 0 && timer <= 1)
-        {
-            //StartCoroutine (wait());
-            head.transform.Rotate(new Vector3(0, 90, 0) * rotationSpeed * Time.deltaTime, Space.World);
-        }*/
-    }
-
-    public void lookingAwayTimer()
-    {
-        timer -= Time.deltaTime; //counting down seconds
-    }
-
-    IEnumerator wait() //waits before turning the head back (not working yet)
-        {
-            int waitTime = Random.Range (5, 10); 
-            yield return new WaitForSeconds (waitTime);
-            head.transform.Rotate(new Vector3(0, -90, 0) * rotationSpeed * Time.deltaTime, Space.World);
-            timer = Random.Range(5, 15);
+            StartCoroutine (WaitTurn());
+            
+            // if (timer < -3)
+            // {
+            //     Debug.Log("timer hit -3");
+            //     head.transform.Rotate(new Vector3(0, -90, 0), Space.World);
+            //     timer = Random.Range(5, 15);
+            // }
         }
+        
+        // coroutine to rotate the head
+        // if (timer > 0) //&& timer <= 1)
+        // {
+        //     StartCoroutine (wait());
+        //     head.transform.Rotate(new Vector3(0, 90, 0), Space.World);
+        // }
+    }
+    
+
+    public void LookingAwayTimer()
+    {
+        if (countdown)
+        {
+            timer -= Time.deltaTime; //counting down seconds
+        }
+    }
+
+    IEnumerator WaitTurn() //waits before turning the head back (not working yet)
+    {
+        timer = 0;
+        countdown = false;
+        // reset head rotation to zero
+        head.transform.rotation = Quaternion.Euler(0,0,0);
+        
+        int waitTime = Random.Range (5, 10);
+        yield return new WaitForSeconds(waitTime);
+            
+        Debug.Log("reset counter");
+        timer = Random.Range(5, 15);
+        countdown = true;
+    }
 }
