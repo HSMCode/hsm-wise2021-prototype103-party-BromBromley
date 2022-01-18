54d893e48c6f8232d1b6edb0d1d70eaad44f30ee
Mia <mia.wittenmeier@students.hs-mainz.de>
2022-01-18 13:48:16 +0100

    add music and audio manager script

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    // this script manages all sounds of the game
+    // attached to the empty object MusicPlayer
+    private AudioSource _soundPlayer;
+    [SerializeField] private AudioClip BackgroundMusic;
+    [SerializeField] private AudioClip evilLaughter;
+    [SerializeField] private AudioClip awwSound;
+    [SerializeField] private AudioClip hmmSound;
+    [SerializeField] private AudioClip diceRoll;
+    
+
+    void Awake()
+    {
+        _soundPlayer = GetComponent<AudioSource>();
+    }
+
+    public void StartMusic()
+    {
+        _soundPlayer.clip = BackgroundMusic;
+        _soundPlayer.Play();
+    }
+
+    public void DiceSound()
+    {
+        _soundPlayer.PlayOneShot(diceRoll, 4f);
+    }
+
+    public void AngrySound()
+    {
+        _soundPlayer.PlayOneShot(hmmSound, 1f);
+    }
+
+    public void LoseSound()
+    {
+        _soundPlayer.PlayOneShot(awwSound, 2f);
+    }
+
+    public void PlayEvilLaughter()
+    {
+        _soundPlayer.PlayOneShot(evilLaughter, 3f);
+    }
+}
